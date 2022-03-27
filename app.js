c6ece9262383a65206b11e9af42917791ca7c736
Austin Olijar <Austin.Olijar@gmail.com>
2022-03-27 12:34:47 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ const container = document.querySelector('.container')
 const display = document.querySelector('.display')
 
 
-console.log(container)
-let movement = 0;
-container.addEventListener('touchmovement', (e) => {
+//let movement = 0;
+container.addEventListener('touchmove', (e) => {
   //  movement += 25;
     //container.style.transform = `translateX(-${movement}%)`
-    const x = e.touches[0].clientX
+    let x = e.touches[0].clientX
     display.innerHTML = x
 })
