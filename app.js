cac913e6df6a449348b41a2a5dd4971138299fe6
Austin Olijar <Austin.Olijar@gmail.com>
2022-03-27 13:16:16 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,30 +1,40 @@
-const container = document.querySelector('.container')
-const displayStart = document.querySelector('.display-start')
-const display = document.querySelector('.display')
-const displayCheck = document.querySelector('.display-check')
-
+const container = document.querySelector(".container");
+const displayStart = document.querySelector(".display-start");
+const display = document.querySelector(".display");
+const displayCheck = document.querySelector(".display-check");
 
 let movement = 0;
 let startX;
 let moveX;
 
-container.addEventListener('touchstart', (e) => {
-    const startX = e.touches[0].clientX
-    displayStart.innerHTML = startX
-})
+container.addEventListener("touchstart", (e) => {
+  startX = e.touches[0].clientX;
+  displayStart.innerHTML = startX;
+});
 
-displayCheck.innerHTML = 'Newest of the update'
-container.addEventListener('touchmove', (e) => {
+displayCheck.innerHTML = "Newest of the update";
+container.addEventListener("touchmove", (e) => {
+  moveX = e.touches[0].clientX;
+  display.innerHTML = moveX;
+});
+
+container.addEventListener("touchend", () => {
+  if (movement >= 100) {
+    movement = 100;
+  } else if (movement <= 0) {
+    movement = 0;
+  }
+
+  if (moveX < startX) {
     movement += 25;
-    //container.style.transform = `translateX(-${movement}%)`
-    moveX = e.touches[0].clientX
-    display.innerHTML = moveX 
-})
-
-container.addEventListener('touchend', () => {
-    if (moveX < startX) {
-        movement += 25;
-        displayCheck.innerHTML = 'Got it'
-        container.style.transform = `translateX(-${movement}%)`
-    }
-})
+    setTimeout(() => {
+      container.style.transform = `translateX(-${movement}%)`;
+    }, 100);
+  }
+  if (moveX > startX) {
+    movement -= 25;
+    setTimeout(() => {
+      container.style.transform = `translateX(-${movement}%)`;
+    }, 100);
+  }
+});
