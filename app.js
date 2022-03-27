a17c7c7555b11af3f1783ac2b7399268332fb3b2
Austin Olijar <Austin.Olijar@gmail.com>
2022-03-27 13:04:46 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -5,30 +5,26 @@ const displayCheck = document.querySelector('.display-check')
 
 
 let movement = 0;
-/*container.addEventListener('touchstart', (e) => {
+let startX;
+let moveX;
+
+container.addEventListener('touchstart', (e) => {
     const startX = e.touches[0].clientX
     displayStart.innerHTML = startX
-})*/
+})
 
-displayCheck.innerHTML = 'New update'
+displayCheck.innerHTML = 'Newest of the update'
 container.addEventListener('touchmove', (e) => {
     movement += 25;
     //container.style.transform = `translateX(-${movement}%)`
-    const startX = e.touches[0].clientX
-    displayStart.innerHTML = startX
-    let x = e.touches[0].clientX
-    display.innerHTML = x 
-  
-    if (x < startX) {
+    moveX = e.touches[0].clientX
+    display.innerHTML = moveX 
+})
+
+container.addEventListener('touchend', () => {
+    if (moveX < startX) {
         movement += 25;
         displayCheck.innerHTML = 'Got it'
         container.style.transform = `translateX(-${movement}%)`
     }
-   
 })
-/*
-container.addEventListener('touchend', (e) => {
-    movement += 25;
-    displayCheck.innerHTML = 'Got it'
-    container.style.transform = `translateX(-${movement}%)`
-})*/
